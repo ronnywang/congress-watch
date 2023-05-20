99bb7806ee191cbd50a20e7349933794d3398294
Ronny Wang <ronnywang@gmail.com>
2023-05-21 00:51:38 +0800

    add CongressWatch::slackQuery($api, $method, $data) and CongressWatch::slackListChannel()

--- init.inc.php
+++ init.inc.php
@@ -17,4 +17,42 @@ class CongressWatch
         }
         return self::$_pdo;
     }
+
+    public static function slackQuery($api, $method = 'GET', $data = null)
+    {
+        $url = sprintf("https://slack.com/api/{$api}");
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            "Authorization: Bearer " . getenv('SLACK_ACCESS_TOKEN'),
+            "Content-Type: application/json; charset=utf-8",
+        ]);
+        if ($method != 'GET') {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        }
+        if (!is_null($data)) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $ret = json_decode(curl_exec($curl));
+        if (!$ret or !property_exists($ret, 'ok') or !$ret->ok) {
+            throw new Exception('slack error: ' . json_encode($ret));
+        }
+        return $ret;
+    }
+
+    public static function slackListChannel()
+    {
+        $limit = 20;
+        $url = "conversations.list?limit={$limit}";
+        while (true) {
+            $ret = CongressWatch::slackQuery($url);
+            foreach ($ret->channels as $channel) {
+                yield $channel;
+            }
+            if (!$ret->response_metadata->next_cursor) {
+                break;
+            }
+            $url = "conversations.list?limit={$limit}&cursor={$ret->response_metadata->next_cursor}";
+        }
+    }
 }
